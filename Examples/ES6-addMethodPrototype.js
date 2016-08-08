a577eabc5525becfb0725c2326d2a6b886069a29
Sebastian Butorovic <sibutorovic@uc.cl>
2016-08-08 13:38:56 -0400

    Update ES6-addMethodPrototype.js

--- Examples/ES6-addMethodPrototype.js
+++ Examples/ES6-addMethodPrototype.js
@@ -2,9 +2,23 @@ function Prefixer(prefix) {
     this.prefix = prefix;
 }
 Prefixer.prototype.prefixArray = function (arr) {
-    return arr.map( function (x) { return this.prefix + x;});
+    return arr.map( function (x) { return this.prefix + x;});       
 };
 
 var pre = new Prefixer('Hi ');
 var prefixed = pre.prefixArray(['Joe', 'Alex']);
-prefixed.forEach(alert);
+prefixed.forEach(alert);                            //Imprime "undefinedJoe, UndefinedAlex"
+
+
+//------------------------------------------------------------------------------------------
+
+function Prefixer(prefix) {
+    this.prefix = prefix;
+}
+Prefixer.prototype.prefixArray = function (arr) {
+    return arr.map( x => { return this.prefix + x;});    //Cambiamos a arrow function
+};
+
+var pre = new Prefixer('Hi ');
+var prefixed = pre.prefixArray(['Joe', 'Alex']);
+prefixed.forEach(alert);                            //Imprime "Hi Joe, Hi Alex"
