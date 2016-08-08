cc0001b3a06e1d88918178517df53a5ce77656f1
Sebastian Butorovic <sibutorovic@uc.cl>
2016-08-08 13:40:11 -0400

    Update ES6-lexicalThisDisadvantage.js

--- Examples/ES6-lexicalThisDisadvantage.js
+++ Examples/ES6-lexicalThisDisadvantage.js
@@ -9,3 +9,18 @@ foo.bar = () => {
 };
 
 foo.bar();  //Imprime "atached to the module"
+
+
+//----------------------------------------------
+
+test = "attached to the module";
+
+var foo = {
+  test: "attached to an object"
+};
+
+foo.bar = function() {                      //Usamos notación function() en vez de arrow functions
+ alert(this.test); 
+};
+
+foo.bar();  //Imprime "atached to the object"
